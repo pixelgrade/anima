63fc9ac1fbed12f56b6f593f10325fab4f655311
Razvan <razwan.me@gmail.com>
2019-09-10 12:22:12 +0300

    configurable header, scroll offset for Header, perf improv on Hero

--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -2,13 +2,26 @@
 
 ?>
 
-<?php get_template_part( 'template-parts/menu-toggle' );  ?>
+<?php get_template_part( 'template-parts/menu-toggle' ); ?>
 
-<header id="masthead" class="site-header">
-	<div class="site-header__inner-container">
-		<div class="site-header__content">
+<header id="masthead" class="site-header site-header--logo-center">
+    <div class="site-header__inner-container">
+        <div class="site-header__content alignfull">
+            <nav class="wp-block-novablocks-navigation">
+				<?php wp_nav_menu( array(
+					'container'      => false,
+					'theme_location' => 'primary',
+					'menu_id'        => 'primary-menu',
+				) ); ?>
+            </nav><!-- #site-navigation -->
 			<?php get_template_part( 'template-parts/site-branding' ); ?>
-			<?php get_template_part( 'template-parts/site-navigation' ); ?>
-		</div>
-	</div>
+            <nav class="wp-block-novablocks-navigation">
+				<?php wp_nav_menu( array(
+					'container'      => false,
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'secondary-menu',
+				) ); ?>
+            </nav><!-- #site-navigation -->
+        </div>
+    </div>
 </header>
--- template-parts/site-navigation.php
+++ template-parts/site-navigation.php
@@ -2,11 +2,4 @@
 
 ?>
 
-<nav id="site-navigation" class="main-navigation">
-	<?php
-	wp_nav_menu( array(
-		'theme_location' => 'menu-1',
-		'menu_id'        => 'primary-menu',
-	) );
-	?>
-</nav><!-- #site-navigation -->
+
