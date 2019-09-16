84781de1e264fb7d43d75543707ec28b340a82b1
madalingorbanescu <madalin@pixelgrade.com>
2019-09-16 11:37:08 +0300

    Cleanup WooCommerce

--- inc/integrations/woocommerce.php
+++ inc/integrations/woocommerce.php
@@ -1,66 +1,94 @@
 <?php
 
+// Remove Add To Cart
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+// Remove Sidebar
 remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+// Remove Breadcrumbs
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+// Remove coupon form
 remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+// Remove product title
 remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
 
 
+// Open wrapper for single product
 add_action( 'woocommerce_before_single_product_summary', 'addStartWrapperBeforeSingleProductSummary', 1 );
+// Close wrapper for single product
 add_action( 'woocommerce_after_single_product_summary', 'addEndWrapperAfterSingleProductSummary', 1 );
+// Open wrapper for tabs
 add_action( 'woocommerce_after_single_product_summary', 'addStartWrapperBeforeTabs', 9 );
+// Close wrapper for tabs
 add_action( 'woocommerce_after_single_product_summary', 'addEndWrapperAfterTabs', 11 );
-// before and after upsells (priority 15)
+// Open wrapper for upselling products
 add_action( 'woocommerce_after_single_product_summary', 'addStartWrapperBeforeUpsells', 14 );
+// Close wrapper for upselling products
 add_action( 'woocommerce_after_single_product_summary', 'addEndWrapperAfterUpsells', 16 );
 
-// before and after related (priority 20)
+// Open wrapper for related products
 add_action( 'woocommerce_after_single_product_summary', 'addStartWrapperBeforeRelated', 19 );
+// Close wrapper for related products
 add_action( 'woocommerce_after_single_product_summary', 'addEndWrapperAfterRelated', 21 );
+// Open wrapper for product on archive
 add_action('woocommerce_before_shop_loop_item', 'addStartWrapperProduct', 20);
+// Close wrapper for product on archive
 add_action('woocommerce_shop_loop_item_title', 'addEndWrapperProduct', 20);
+// Change markup for product title on archive
 add_action('woocommerce_shop_loop_item_title','newProductTitleMarkup', 10);
 
+// Open wrapper for product price
 add_action('woocommerce_shop_loop_item_title', 'addStartWrapperPrice', 30);
+// Close wrapper for product price
 add_action('woocommerce_after_shop_loop_item_title', 'addEndWrapperPrice', 30);
 
+// Open wrapper for single product (container)
 add_action( 'woocommerce_before_main_content', 'addStartMainContent', 21 );
+// Close wrapper for single product (container)
 add_action( 'woocommerce_after_main_content', 'addEndMainContent', 21 );
 
+// Add ajax button to products
 add_action( 'woocommerce_after_add_to_cart_quantity', 'outputAjaxAddToCartButton' );
 
+// Add breadcrumbs on single product, right before title
 add_action( 'woocommerce_single_product_summary', 'woocommerce_breadcrumb', 1 );
 
+// Add shop categories on archive, right after title
 add_action( 'woocommerce_archive_description', 'woocommerce_display_categories', 1 );
-
-// hide tabs content titles
-add_filter( 'woocommerce_product_description_heading', '__return_false', 30 );
-add_filter( 'woocommerce_product_additional_information_heading', '__return_false', 30 );
 
 // Add Site title and Breadcrumbs to Checkout Page
 add_action( 'woocommerce_checkout_billing', 'outputCheckoutSiteIdentity', 1 );
 add_action( 'woocommerce_checkout_billing', 'outputCheckoutBreadcrumbs', 2 );
+
+// Move coupon form between Site Title and Checkout Form
 add_action( 'woocommerce_checkout_billing', 'woocommerce_checkout_coupon_form', 10 );
 
-add_filter( 'woocommerce_pagination_args', 'rosa_woocommerce_pagination_args', 40, 1 );
+// Add hidden coupon form outside checkout form
+add_action( 'woocommerce_before_checkout_form', 'woocommerceCouponForm', 10 );
 
-// Add coupon form outside checkout form
-add_action( 'woocommerce_before_checkout_form', 'woocommerceCoponForm', 10 );
-
-// Limit related posts number
-add_filter( 'woocommerce_output_related_products_args', 'limitRelatedPostsCount', 20 );
-
-// Limit upsell products number
-add_filter( 'woocommerce_upsell_display_args', 'limitRelatedPostsCount', 20 );
-
+// Add ajax button to archive products
 add_action('woocommerce_after_shop_loop_item', 'appendAddToCartButton', 1);
 
-
+// Output fly-out cart markup
 add_action( 'rosa_before_header', 'outputMiniCart', 1 );
 
 // Append cart to menu
 add_filter( 'wp_nav_menu_items', 'appendCartIconToMenu', 10, 2 );
+
+// Hide tabs content titles
+add_filter( 'woocommerce_product_description_heading', '__return_false', 30 );
+add_filter( 'woocommerce_product_additional_information_heading', '__return_false', 30 );
+
+// Append cart to menu
+add_filter( 'wp_nav_menu_items', 'appendCartIconToMenu', 10, 2 );
+
+// Limit related posts number
+add_filter( 'woocommerce_output_related_products_args', 'limitRelatedPostsCount', 20 );
+
+// Limit upselling products number
+add_filter( 'woocommerce_upsell_display_args', 'limitRelatedPostsCount', 20 );
+
+// Change args for WooCommerce pagination
+add_filter( 'woocommerce_pagination_args', 'rosa_woocommerce_pagination_args', 40, 1 );
 
 function addStartWrapperBeforeSingleProductSummary() {
 	echo '<div class="c-product-main">';
@@ -268,8 +296,8 @@ if ( ! function_exists( 'woocommerce_display_categories' ) ) {
 	}
 }
 
-if ( ! function_exists( 'woocommerceCoponForm' ) ) {
-	function woocommerceCoponForm() {
+if ( ! function_exists( 'woocommerceCouponForm' ) ) {
+	function woocommerceCouponForm() {
 		echo '<form class="checkout_coupon woocommerce-form-coupon" id="form-coupon" method="post" style="display:none"></form>';
 	}
 }
