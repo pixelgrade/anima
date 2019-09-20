37745b02b883705632d12a3c19baf9fbc0bbeac2
Razvan <razwan.me@gmail.com>
2019-09-20 18:51:58 +0300

    cooler buttons

--- src/blocks/button/index.js
+++ src/blocks/button/index.js
@@ -1,12 +1,13 @@
 wp.domReady( () => {
 
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
 
 	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'primary',
 		label: 'Primary',
+		isDefault: true,
 	} );
 
 	wp.blocks.registerBlockStyle( 'core/button', {
