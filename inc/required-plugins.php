bfe8e7b2b00c2dac2ace2152ce8a7f42cb61445d
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-10-23 10:57:06 +0300

    Adjustments to required plugins

--- inc/required-plugins.php
+++ inc/required-plugins.php
@@ -29,21 +29,20 @@ function rosa2_register_required_plugins() {
 			'required'           => true,
 			'source'             => $protocol . '//wupdates.com/api_wupl_version/JxbVe/2v5t1czd3vw4kmb5xqmyxj1kkwmnt9q0463lhj393r5yxtshdyg05jssgd4jglnfx7A2vdxtfdcf78r9r1sm217k4ht3r2g7pkdng5f6tgwyrk23wryA0pjxvs7gwhhb',
 			'external_url'       => $protocol . '//github.com/pixelgrade/pixelgrade_care',
-			'version'            => '1.6.2',
-			'is_automatic'       => true
+			'version'            => '1.6.4'
 		),
 		array(
 			'name'     => 'Customify',
 			'slug'     => 'customify',
 			'is_callable' => 'PixCustomifyPlugin',
 			'required' => true,
-			'version'  => '2.5.3',
+			'version'  => '2.5.4',
 		),
 		array(
 			'name'     => 'Nova Blocks',
 			'slug'     => 'nova-blocks',
 			'required' => true,
-			'version'  => '1.1.0',
+			'version'  => '1.1.4',
 		),
 		array(
 			'name'     => 'Gutenberg',
