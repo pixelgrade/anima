48e88c0565826a7e9e6153ff15f1e348120022b9
madalingorbanescu <madalin@pixelgrade.com>
2020-02-03 12:03:46 +0200

    Fix more entry-content mess

--- archive.php
+++ archive.php
@@ -18,23 +18,23 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) { ?>
 
-            <header class="entry-header">
-                <div class="entry-content has-text-align-center">
-                    <?php
-                    the_archive_title( '<h1 class="page-title">', '</h1>' );
-                    the_archive_description( '<div class="archive-description">', '</div>' );
-                    ?>
-                </div>
-	        </header><!-- .page-header -->
+                <header class="entry-header">
+                    <div class="entry-content has-text-align-center">
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+                    </div>
+                </header><!-- .page-header -->
 
-	        <div class="entry-content">
-		        <?php
+                <div class="entry-content">
+					<?php
 					get_template_part( 'template-parts/loop' );
-					rosa2_the_posts_pagination();
-				} else {
-					get_template_part( 'template-parts/content', 'none' );
-				} ?>
-	        </div>
+					rosa2_the_posts_pagination(); ?>
+                </div>
+			<?php } else {
+				get_template_part( 'template-parts/content', 'none' );
+			} ?>
 
         </main><!-- #main -->
     </div><!-- #primary -->
