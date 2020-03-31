c414924acfae4706b138e8b03d5b287d7cfa9032
Razvan <razwan.me@gmail.com>
2020-03-31 13:13:54 +0300

    Modify slope adjust to match frontend and backend

--- inc/integrations/novablocks.php
+++ inc/integrations/novablocks.php
@@ -106,11 +106,11 @@ if ( ! function_exists( 'rosa2_alter_novablocks_media_settings' ) ) {
 
 		$settings['media']['attributes']['horizontalAlignment']['default'] = 'center';
 
-		if ( ! empty( $settings['media']['blockAreaOptions'] ) ) {
-			$settings['media']['blockAreaOptions'] = array_filter( $settings['media']['blockAreaOptions'], function ( $option ) {
-				return $option['value'] != 'highlighted';
-			} );
-		}
+//		if ( ! empty( $settings['media']['blockAreaOptions'] ) ) {
+//			$settings['media']['blockAreaOptions'] = array_filter( $settings['media']['blockAreaOptions'], function ( $option ) {
+//				return $option['value'] != 'highlighted';
+//			} );
+//		}
 
 		return $settings;
 	}
