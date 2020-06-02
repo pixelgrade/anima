3d16d6ec747734db6db46b01bacb16c939230df4
Razvan <razwan.me@gmail.com>
2020-06-02 16:45:16 +0300

    Minor tweaks to blog archive media cards spacing

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,13 +15,15 @@ global $wp_query;
 
 $classes = array(
 	'novablocks-media',
-	'novablocks-block',
 	'novablocks-media--blog',
 	'wp-block-group',
 	'alignfull',
+);
+
+$blockClasses = array(
+	'novablocks-block',
 	'content-is-moderate',
 	'block-is-moderate',
-	'has-background',
 );
 
 if ( ! has_post_thumbnail() ) {
@@ -33,28 +35,30 @@ $classes[] = ( $wp_query->current_post % 2 ) ? 'has-image-on-the-right' : 'has-i
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-    <div class="wp-block-group__inner-container">
-        <div class="wp-block alignwide">
-            <div class="novablocks-media__layout">
-                <div class="novablocks-media__content">
-                    <div class="novablocks-media__inner-container novablocks-block__content">
-                        <?php
-                        get_template_part( 'template-parts/entry-meta', get_post_type() );
-                        the_title( '<h2 class="entry-title has-text-align-center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-                        rosa2_the_separator( 'decorative' );
-                        echo '<p class="has-text-align-center">' . get_the_excerpt() . '</p>';
-                        rosa2_the_read_more_button();
-                        ?>
+    <div class="<?php echo esc_attr( join( ' ', $blockClasses ) ); ?>">
+        <div class="wp-block-group__inner-container">
+            <div class="wp-block alignwide">
+                <div class="novablocks-media__layout">
+                    <div class="novablocks-media__content">
+                        <div class="novablocks-media__inner-container novablocks-block__content">
+                            <?php
+                            get_template_part( 'template-parts/entry-meta', get_post_type() );
+                            the_title( '<h2 class="entry-title has-text-align-center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                            rosa2_the_separator( 'decorative' );
+                            echo '<p class="has-text-align-center">' . get_the_excerpt() . '</p>';
+                            rosa2_the_read_more_button();
+                            ?>
+                        </div>
                     </div>
-                </div>
-                <div class="novablocks-media__aside">
-                    <div class="novablocks-media__image">
-	                    <?php
-	                    if( has_post_thumbnail() ) {
-		                    rosa2_post_thumbnail();
-	                    } else { ?>
-		                    <svg viewBox="-100 -100 344 344" width="144" height="144" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M84 36L61.5 66l17.1 22.8L69 96C58.86 82.5 42 60 42 60L6 108h132L84 36z" fill="#212B49"/></svg>
-	                    <?php } ?>
+                    <div class="novablocks-media__aside">
+                        <div class="novablocks-media__image">
+                            <?php
+                            if( has_post_thumbnail() ) {
+                                rosa2_post_thumbnail();
+                            } else { ?>
+                                <svg viewBox="-100 -100 344 344" width="144" height="144" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M84 36L61.5 66l17.1 22.8L69 96C58.86 82.5 42 60 42 60L6 108h132L84 36z" fill="#212B49"/></svg>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
             </div>
