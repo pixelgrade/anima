c83c39790b5bf03445b599a43b1e309a66737e17
madalingorbanescu <madalin@pixelgrade.com>
2020-10-09 17:56:58 +0300

    Remove Aria Label

--- inc/extras.php
+++ inc/extras.php
@@ -436,7 +436,7 @@ function rosa2_custom_search_form( $form ) {
 
     $placeholder = 'Search ' . esc_html( get_bloginfo( 'name' ) ) . ' ...';
 
-	$form = '<form role="search" ' . $aria_label . 'method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+	$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
 				<label>
 					<span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>
 					<input type="search" class="search-field" placeholder="' . esc_attr_x( $placeholder, 'placeholder' ) . '" value="' . get_search_query() . '" name="s" />
