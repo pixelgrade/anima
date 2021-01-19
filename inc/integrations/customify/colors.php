1e1a8d4a651b9961ebc62a02dccfbcb03c66c7a5
Razvan <razwan.me@gmail.com>
2021-01-19 18:06:52 +0200

    Add back palette colors for backwards compatibility reasons

--- inc/integrations/customify/colors.php
+++ inc/integrations/customify/colors.php
@@ -100,6 +100,168 @@ function pixelgrade_add_colors_section_to_customify_config( $config ) {
 	$config['sections']['colors_section'] = Customify_Array::array_merge_recursive_distinct( $config['sections']['colors_section'], array(
 		'title'   => esc_html__( 'Colors', '__theme_txtd' ),
 		'options' => array(
+			'sm_color_primary'   => array(
+				'default'          => THEME_COLOR_PRIMARY,
+				'connected_fields' => array(
+					'color_1'
+				),
+			),
+			'sm_color_secondary' => array(
+				'default'          => THEME_COLOR_SECONDARY,
+				'connected_fields' => array(
+					'color_2'
+				),
+			),
+			'sm_color_tertiary'  => array(
+				'default'          => THEME_COLOR_TERTIARY,
+				'connected_fields' => array(
+					'color_3'
+				),
+			),
+			'sm_dark_primary'    => array(
+				'default'          => THEME_DARK_PRIMARY,
+				'connected_fields' => array(
+					'color_dark_1'
+				),
+			),
+			'sm_dark_secondary'  => array(
+				'default'          => THEME_DARK_SECONDARY,
+				'connected_fields' => array(
+					'color_dark_2'
+				),
+			),
+			'sm_dark_tertiary'   => array(
+				'default'          => THEME_DARK_TERTIARY,
+				'connected_fields' => array(
+					'color_dark_3'
+				),
+			),
+			'sm_light_primary'   => array(
+				'default'          => THEME_LIGHT_PRIMARY,
+				'connected_fields' => array(
+					'color_light_1'
+				),
+			),
+			'sm_light_secondary' => array(
+				'default'          => THEME_LIGHT_SECONDARY,
+				'connected_fields' => array(
+					'color_light_2'
+				),
+			),
+			'sm_light_tertiary'  => array(
+				'default'          => THEME_LIGHT_TERTIARY,
+				'connected_fields' => array(
+					'color_light_3'
+				),
+			),
+			'color_1'       => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Primary Color', '__theme_txtd' ),
+				'default' => THEME_COLOR_PRIMARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-color-primary',
+					),
+				),
+			),
+			'color_2'       => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Secondary Color', '__theme_txtd' ),
+				'default' => THEME_COLOR_SECONDARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-color-secondary',
+					),
+				),
+			),
+			'color_3'       => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Tertiary Color', '__theme_txtd' ),
+				'default' => THEME_COLOR_TERTIARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-color-tertiary',
+					),
+				),
+			),
+			'color_dark_1'  => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Primary Dark Color', '__theme_txtd' ),
+				'default' => THEME_DARK_PRIMARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-dark-primary',
+					),
+				),
+			),
+			'color_dark_2'  => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Secondary Dark Color', '__theme_txtd' ),
+				'default' => THEME_DARK_SECONDARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-dark-secondary',
+					),
+				),
+			),
+			'color_dark_3'  => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Tertiary Dark Color', '__theme_txtd' ),
+				'default' => THEME_DARK_TERTIARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-dark-tertiary',
+					),
+				),
+			),
+			'color_light_1' => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Primary Light Color', '__theme_txtd' ),
+				'default' => THEME_LIGHT_PRIMARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-light-primary',
+					),
+				),
+			),
+			'color_light_2' => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Secondary Light Color', '__theme_txtd' ),
+				'default' => THEME_LIGHT_SECONDARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-light-secondary',
+					),
+				),
+			),
+			'color_light_3' => array(
+				'type'    => 'color',
+				'live'    => true,
+				'label'   => esc_html__( 'Theme Tertiary Light Color', '__theme_txtd' ),
+				'default' => THEME_LIGHT_TERTIARY,
+				'css'     => array(
+					array(
+						'selector' => ':root',
+						'property' => '--theme-light-tertiary',
+					),
+				),
+			),
 
 			'body_color' => sm_get_color_switch_dark_config( 'Body', 'body', 'off' ),
 
