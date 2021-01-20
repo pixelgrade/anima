4deda92b2cf07c93f1fe9d3129a6e3d85191ebe7
Razvan <razwan.me@gmail.com>
2021-01-20 12:34:04 +0200

    Remove old color palettes integration config

--- inc/integrations/customify/colors.php
+++ inc/integrations/customify/colors.php
@@ -100,169 +100,6 @@ function pixelgrade_add_colors_section_to_customify_config( $config ) {
 	$config['sections']['colors_section'] = Customify_Array::array_merge_recursive_distinct( $config['sections']['colors_section'], array(
 		'title'   => esc_html__( 'Colors', '__theme_txtd' ),
 		'options' => array(
-			'sm_color_primary'   => array(
-				'default'          => THEME_COLOR_PRIMARY,
-				'connected_fields' => array(
-					'color_1'
-				),
-			),
-			'sm_color_secondary' => array(
-				'default'          => THEME_COLOR_SECONDARY,
-				'connected_fields' => array(
-					'color_2'
-				),
-			),
-			'sm_color_tertiary'  => array(
-				'default'          => THEME_COLOR_TERTIARY,
-				'connected_fields' => array(
-					'color_3'
-				),
-			),
-			'sm_dark_primary'    => array(
-				'default'          => THEME_DARK_PRIMARY,
-				'connected_fields' => array(
-					'color_dark_1'
-				),
-			),
-			'sm_dark_secondary'  => array(
-				'default'          => THEME_DARK_SECONDARY,
-				'connected_fields' => array(
-					'color_dark_2'
-				),
-			),
-			'sm_dark_tertiary'   => array(
-				'default'          => THEME_DARK_TERTIARY,
-				'connected_fields' => array(
-					'color_dark_3'
-				),
-			),
-			'sm_light_primary'   => array(
-				'default'          => THEME_LIGHT_PRIMARY,
-				'connected_fields' => array(
-					'color_light_1'
-				),
-			),
-			'sm_light_secondary' => array(
-				'default'          => THEME_LIGHT_SECONDARY,
-				'connected_fields' => array(
-					'color_light_2'
-				),
-			),
-			'sm_light_tertiary'  => array(
-				'default'          => THEME_LIGHT_TERTIARY,
-				'connected_fields' => array(
-					'color_light_3'
-				),
-			),
-			'color_1'       => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Primary Color', '__theme_txtd' ),
-				'default' => THEME_COLOR_PRIMARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-color-primary',
-					),
-				),
-			),
-			'color_2'       => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Secondary Color', '__theme_txtd' ),
-				'default' => THEME_COLOR_SECONDARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-color-secondary',
-					),
-				),
-			),
-			'color_3'       => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Tertiary Color', '__theme_txtd' ),
-				'default' => THEME_COLOR_TERTIARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-color-tertiary',
-					),
-				),
-			),
-			'color_dark_1'  => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Primary Dark Color', '__theme_txtd' ),
-				'default' => THEME_DARK_PRIMARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-dark-primary',
-					),
-				),
-			),
-			'color_dark_2'  => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Secondary Dark Color', '__theme_txtd' ),
-				'default' => THEME_DARK_SECONDARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-dark-secondary',
-					),
-				),
-			),
-			'color_dark_3'  => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Tertiary Dark Color', '__theme_txtd' ),
-				'default' => THEME_DARK_TERTIARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-dark-tertiary',
-					),
-				),
-			),
-			'color_light_1' => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Primary Light Color', '__theme_txtd' ),
-				'default' => THEME_LIGHT_PRIMARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-light-primary',
-					),
-				),
-			),
-			'color_light_2' => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Secondary Light Color', '__theme_txtd' ),
-				'default' => THEME_LIGHT_SECONDARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-light-secondary',
-					),
-				),
-			),
-			'color_light_3' => array(
-				'type'    => 'color',
-				'live'    => true,
-				'label'   => esc_html__( 'Theme Tertiary Light Color', '__theme_txtd' ),
-				'default' => THEME_LIGHT_TERTIARY,
-				'css'     => array(
-					array(
-						'selector' => ':root',
-						'property' => '--theme-light-tertiary',
-					),
-				),
-			),
-
 			'body_color' => sm_get_color_switch_dark_config( 'Body', 'body', 'off' ),
 
 			'colors_links_section_title' => array(
@@ -270,7 +107,7 @@ function pixelgrade_add_colors_section_to_customify_config( $config ) {
 				'html' => '<span class="separator sub-section label">' . esc_html__( 'Links', '__theme_txtd' ) . '</span>',
 			),
 			'links_color' => sm_get_color_switch_dark_config( 'Link', ':root', 'off', '--theme-links-color' ),
-            'links_decoration_color' => sm_get_color_select_dark_config( 'Link Decoration', ':root', 'text', '--theme-links-decoration-color' ),
+            'links_decoration_color' => sm_get_color_select_dark_config( 'Link Decoration', ':root', 'dark', '--theme-links-decoration-color' ),
             'links_hover_color' => sm_get_color_switch_dark_config( 'Active Link', ':root', 'off', '--theme-links-hover-color' ),
             'links_hover_decoration_color' => sm_get_color_select_dark_config( 'Active Link Decoration', ':root', 'accent', '--theme-links-hover-decoration-color' ),
 
@@ -299,7 +136,7 @@ function pixelgrade_add_colors_section_to_customify_config( $config ) {
 				'type' => 'html',
 				'html' => '<span class="separator sub-section label">' . esc_html__( 'Buttons', '__theme_txtd' ) . '</span>',
 			),
-			'text_button' => sm_get_color_switch_dark_config( 'Text Button Color', '.wp-block-button.is-style-text .wp-block-button__link', 'off' ),
+			'text_button' => sm_get_color_switch_dark_config( 'Text Button Color', '.wp-block-button.is-style-text .wp-block-button__link', 'off', array( '--theme-button-text-color' ) ),
 			'solid_button' => sm_get_color_switch_dark_config( 'Solid Button Color', ':root', 'off', '--sm-button-background-color' ),
 
 			'colors_header_section_title' => array(
