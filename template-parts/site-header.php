48d46edf36bb86d3186ed00dec7fb5c723aa4262
madalingorbanescu <madalin@pixelgrade.com>
2021-02-11 09:52:33 +0200

    Update site header template and update css fallback

--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -11,31 +11,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_template_part( 'template-parts/menu-toggle' ); ?>
+get_template_part( 'template-parts/menu-toggle' );
+?>
 
-<header id="masthead" class="site-header site-header--logo-center">
+<header id="masthead" class="site-header site-header--main site-header--logo-center">
     <div class="site-header__wrapper">
         <div class="site-header__inner-container">
             <div class="site-header__content alignfull">
-                <nav class="wp-block-novablocks-navigation site-header__menu site-header__menu--secondary">
-                    <?php wp_nav_menu( array(
-                        'container'      => false,
-                        'theme_location' => 'secondary',
-                        'menu_id'        => 'secondary-menu',
-                        'fallback_cb'    => false,
-                    ) ); ?>
-                </nav><!-- #site-navigation -->
+                <div class="wp-block-novablocks-header-row site-header__row site-header__row--primary">
+                    <nav class="wp-block-novablocks-navigation site-header__menu site-header__menu--secondary">
+                        <?php wp_nav_menu( array(
+                            'container'      => false,
+                            'theme_location' => 'secondary',
+                            'menu_id'        => 'secondary-menu',
+                            'fallback_cb'    => false,
+                        ) ); ?>
+                    </nav><!-- #site-navigation -->
 
-                <?php get_template_part( 'template-parts/site-branding' ); ?>
+                    <?php get_template_part( 'template-parts/site-branding' ); ?>
 
-                <nav class="wp-block-novablocks-navigation site-header__menu site-header__menu--primary">
-                    <?php wp_nav_menu( array(
-                        'container'      => false,
-                        'theme_location' => 'primary',
-                        'menu_id'        => 'primary-menu',
-                        'fallback_cb'    => false,
-                    ) ); ?>
-                </nav><!-- #site-navigation -->
+                    <nav class="wp-block-novablocks-navigation site-header__menu site-header__menu--primary">
+                        <?php wp_nav_menu( array(
+                            'container'      => false,
+                            'theme_location' => 'primary',
+                            'menu_id'        => 'primary-menu',
+                            'fallback_cb'    => false,
+                        ) ); ?>
+                    </nav><!-- #site-navigation -->
+                </div>
             </div>
         </div>
     </div>
