1ce6bab50e8d6828cb88c0c154a02a656e4ed4c4
madalingorbanescu <madalin@pixelgrade.com>
2021-05-04 19:53:17 +0300

    Progress on editor

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $page_content_block =
-	'<!-- wp:novablocks/sidecar {"layout":"no-sidebar", "className":"ignore-block"} -->' .
+	'<!-- wp:novablocks/sidecar {"layout":"no-sidebar", "sidebarWidth": "large", "className":"ignore-block"} -->' .
 	'<!-- wp:novablocks/sidecar-area {"className":"novablocks-content entry-content"} -->' .
 	rosa2_get_content_markup().
 	'<!-- /wp:novablocks/sidecar-area -->' .
