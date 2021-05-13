8a528e1df3a94f2cbaeb7c397d553365e4eeeaac
madalingorbanescu <madalin@pixelgrade.com>
2021-05-13 17:08:06 +0300

    New meta for single articles

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,19 +11,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-$article_thumbnail_type = rosa2_get_image_aspect_ratio_type( get_post_thumbnail_id( $post ), 'landscape' );
-
-$article_landscape_markup = '<!-- wp:novablocks/sidecar { "className":"alignwide", "sidebarWidth":"medium"} -->' .
-                            '<!-- wp:novablocks/sidecar-area {"className":"novablocks-content"} -->' .
-                            '<div class="article-header article-header--landscape alignwide">' .
-                            '<div class="entry-header">' .
-                            rosa2_get_meta_template_part() .
-                            '<h1 class="entry-title">' . get_the_title() . '</h1>' .
-                            '</div>' .
-                            '</div>' .
-                            '<!-- wp:novablocks/sidecar { "className":"alignwide", "sidebarWidth":"medium"} -->' .
+$article_markup = '          <!-- wp:novablocks/sidecar { "className":"alignwide", "sidebarWidth":"medium"} -->' .
                             '<!-- wp:novablocks/sidecar-area {"className":"novablocks-content entry-content"} -->' .
+                            '<div class="article-header article-header--landscape">' .
+                            '<div class="entry-header">' .
+                            rosa2_get_categories_markup() .
+                            '<h1 class="entry-title">' . get_the_title() . '</h1>' .
+                            '<p class="entry-excerpt">' . get_the_excerpt() . '</p>' .
+                            rosa2_get_meta_template_part() .
+                            '</div>' .
                             rosa2_get_thumbnail_markup() .
+                            '</div>' .
                             rosa2_get_content_markup() .
                             rosa2_get_post_navigation_markup() .
                             '<!-- wp:novablocks/post-comments --><!-- /wp:novablocks/post-comments -->' .
@@ -31,40 +29,7 @@ $article_landscape_markup = '<!-- wp:novablocks/sidecar { "className":"alignwide
                             '<!-- wp:novablocks/sidecar-area {"className":"novablocks-sidebar"} -->' .
                             rosa2_get_sidebar_markup() .
                             '<!-- /wp:novablocks/sidecar-area -->' .
-                            '<!-- /wp:novablocks/sidecar -->' .
-                            '<!-- /wp:novablocks/sidecar-area -->' .
                             '<!-- /wp:novablocks/sidecar -->';
-
-$article_portrait_markup = '<!-- wp:novablocks/sidecar { "className":"alignwide", "sidebarWidth":"medium"} -->' .
-                           '<!-- wp:novablocks/sidecar-area {"className":"novablocks-content"} -->' .
-                           '<div class="article-header article-header--portrait alignwide">' .
-                           rosa2_get_thumbnail_markup() .
-                           '<div>' .
-                           '<div class="entry-header">' .
-                           rosa2_get_meta_template_part() .
-                           '<h1 class="entry-title">' . get_the_title() . '</h1>' .
-                           '</div>' .
-                           '<p class="post-excerpt">' . get_the_excerpt() . '</p>' .
-                           '</div>' .
-                           '</div>' .
-                           '<!-- wp:novablocks/sidecar { "className":"alignwide", "sidebarWidth":"medium"} -->' .
-                           '<!-- wp:novablocks/sidecar-area {"className":"novablocks-content entry-content"} -->' .
-                           rosa2_get_content_markup() .
-                           rosa2_get_post_navigation_markup() .
-                           '<!-- wp:novablocks/post-comments --><!-- /wp:novablocks/post-comments -->' .
-                           '<!-- /wp:novablocks/sidecar-area -->' .
-                           '<!-- wp:novablocks/sidecar-area {"className":"novablocks-sidebar"} -->' .
-                           rosa2_get_sidebar_markup() .
-                           '<!-- /wp:novablocks/sidecar-area -->' .
-                           '<!-- /wp:novablocks/sidecar -->' .
-                           '<!-- /wp:novablocks/sidecar-area -->' .
-                           '<!-- /wp:novablocks/sidecar -->';
-
-$article_markup = $article_landscape_markup;
-
-if ( $article_thumbnail_type === 'portrait' ) {
-	$article_markup = $article_portrait_markup;
-}
 
 ?>
 
