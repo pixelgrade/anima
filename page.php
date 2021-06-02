a3813ef1c3c2e9a114439bb36bae32ada3a375d1
madalingorbanescu <madalin@pixelgrade.com>
2021-06-02 13:33:18 +0300

    Minor change to page and single markup

--- page.php
+++ page.php
@@ -22,12 +22,11 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+            while ( have_posts() ) {
+                the_post();
+                get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
-
-		endwhile; // End of the loop.
+            }
 		?>
 
 		</main><!-- #main -->
--- single.php
+++ single.php
@@ -15,6 +15,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+
 			<?php
 			    while ( have_posts() ) {
 				    the_post();
