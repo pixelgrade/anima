0d5cc515bfefc0af15813d8574af2c29f339594f
madalingorbanescu <madalin@pixelgrade.com>
2021-06-04 09:09:32 +0300

    Temporary migration

--- inc/upgrade/migrations/1.11.0-update-content-width-setting-value.php
+++ inc/upgrade/migrations/1.11.0-update-content-width-setting-value.php
@@ -4,38 +4,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$rosa2_options = get_theme_mod( 'rosa2_options', array() );
+$config = apply_filters( 'customify_filter_fields', array() );
 
-if ( empty( $rosa2_options ) || ! is_array( $rosa2_options ) ) {
-	return;
+$content_option = pixelgrade_option('content_width', 30);
+$container_option = pixelgrade_option('content_wide_width_addon', 35);
+
+$new_container_option_value = $content_option + $container_option;
+
+// New maximum setting for Container Width is 100,
+// so we are going to limit new value to that.
+if ( $new_container_option_value > 100 ) {
+	$new_container_option_value = '100';
 }
 
-if ( ! empty( $rosa2_options['content_width'] ) && ! empty( $rosa2_options['content_wide_width_addon'] ) ) {
-
-	// Content Width
-	$content_width_addon   = $rosa2_options['content_width'];
-	// Old Container Width
-	$container_width_addon = $rosa2_options['content_wide_width_addon'];
-
-	// New Container Width should be equal with Content Width + Old Container Width.
-	$new_container_width_addon = strval( $content_width_addon + $container_width_addon );
-
-	// New maximum setting for Container Width is 100,
-	// so we are going to limit new value to that.
-	if ( $new_container_width_addon > 100 ) {
-		$new_container_width_addon = '100';
-	}
-
-	// New minimum setting for Container Width is 60,
-	// so we are going to limit new value to that.
-	if ( $new_container_width_addon < 60 ) {
-		$new_container_width_addon = '60';
-	}
-
-	// Update content_wide_width_addon value.
-	if ( isset( $rosa2_options['content_width'] ) && isset( $rosa2_options['content_wide_width_addon'] ) ) {
-		$rosa2_options['content_wide_width_addon'] = $new_container_width_addon;
-	}
+// New minimum setting for Container Width is 60,
+// so we are going to limit new value to that.
+if ( $new_container_option_value < 60 ) {
+	$new_container_option_value = '60';
 }
 
-set_theme_mod( 'rosa2_options', $rosa2_options );
+$container_option_config = pixelgrade_get_option_customizer_config('content_wide_width_addon', $config);
+
+if ( ! empty( $container_option_config ) && isset( $container_option_config['setting_type'] ) && 'option' === $container_option_config['setting_type'] ) {
+
+	if ( ! empty( $option_config['setting_id'] ) ) {
+		$setting_id = $option_config['setting_id'];
+	} else {
+		$setting_id = $config['opt-name'] . '[content_wide_width_addon]';
+	}
+
+	update_option( $setting_id, $new_container_option_value );
+} else {
+
+	if ( PixCustomifyPlugin()->settings->get_plugin_setting('values_store_mod') === 'option' ) {
+		$setting_id = $config['opt-name'] . '[content_wide_width_addon]';
+		update_option( $setting_id, $new_container_option_value );
+	} else {
+		$rosa2_options = get_theme_mod( 'rosa2_options' );
+		$rosa2_options['content_wide_width_addon'] = strval( $new_container_option_value );
+		set_theme_mod( 'rosa2_options', $rosa2_options );
+	}
+}
