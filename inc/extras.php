0aa26c809903acf5834fcd3d3cf61e1d81e745e3
madalingorbanescu <madalin@pixelgrade.com>
2021-07-15 15:47:54 +0300

    Move Home content markup

--- inc/extras.php
+++ inc/extras.php
@@ -474,7 +474,7 @@ if ( ! function_exists( 'rosa2_article_header' ) ) {
 	}
 }
 
-if ( ! function_exists( 'rosa2_get_archive_content')) {
+if ( ! function_exists( 'rosa2_get_archive_content' ) ) {
 
 	function rosa2_get_archive_content() {
 
@@ -491,6 +491,56 @@ if ( ! function_exists( 'rosa2_get_archive_content')) {
 			<?php
 			get_template_part( 'template-parts/loop' );
 			rosa2_the_posts_pagination();
+		} else {
+			get_template_part( 'template-parts/content', 'none' );
+		}
+
+		return ob_get_clean();
+	}
+}
+
+if ( ! function_exists( 'rosa2_get_home_content_markup' ) ) {
+
+	function rosa2_get_home_content_markup() {
+
+		ob_start();
+
+		$page_for_posts = get_option( 'page_for_posts' );
+		$categories = get_categories();
+
+		if ( ! empty( $categories ) ) {
+			$categories = array_filter( $categories, function ( $category ) {
+				return $category->term_id !== 1;
+			} );
+		}
+
+		$has_title      = ! empty( $page_for_posts );
+		$has_categories = ! empty( $categories ) && ! is_wp_error( $categories );
+
+		if ( have_posts() ) {
+			if ( $has_title || $has_categories ) { ?>
+                <header class="entry-header has-text-align-center entry-content">
+					<?php
+
+					if ( $has_title ) {
+						echo '<h1 class="page-title">' . get_the_title( $page_for_posts ) . '</h1>';
+					}
+
+					if ( $has_categories ) {
+						echo '<ul class="entry-meta">';
+						foreach ( $categories as $category ) {
+							$category_url = get_category_link( $category->term_id );
+							echo '<li><a href="' . esc_url( $category_url ) . '">' . esc_html( $category->name ) . '</a></li>';
+						}
+						echo '</ul>';
+					}
+					?>
+                </header><!-- .page-header -->
+			<?php }
+
+			get_template_part( 'template-parts/loop' );
+			rosa2_the_posts_pagination();
+
 		} else {
 			get_template_part( 'template-parts/content', 'none' );
 		}
