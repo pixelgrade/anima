1de9ec9a2af7c4327973db2cb2a20995216ae0ea
madalingorbanescu <madalin@pixelgrade.com>
2021-07-27 16:56:58 +0300

    Recompile some deleted scripts

--- dist/js/admin/customizer-nav-menus.js
+++ dist/js/admin/customizer-nav-menus.js
@@ -81,12 +81,12 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 16);
+/******/ 	return __webpack_require__(__webpack_require__.s = 20);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 16:
+/***/ 20:
 /***/ (function(module, exports) {
 
 /**
