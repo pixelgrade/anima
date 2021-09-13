3f6e71c04b882b56aae806557f1d494d799dd628
Vlad Olaru <vlad@pixelgrade.com>
2021-09-13 15:21:18 +0300

    Upgraded NovaBlocks integration

--- inc/integrations/novablocks.php
+++ inc/integrations/novablocks.php
@@ -21,36 +21,72 @@ add_filter( 'novablocks_block_editor_settings', 'anima_alter_novablocks_map_sett
 if ( ! function_exists( 'anima_novablocks_setup' ) ) {
 	function anima_novablocks_setup() {
 		$anima_novablocks_config = array(
-			'doppler' => array(
-				'novablocks/hero',
-			),
-			'blobs' => array(
-				'novablocks/media',
-				'novablocks/advanced-gallery',
-			),
-			'sharing-overlay',
-			'advanced-gallery',
-			'announcement-bar',
-			'cards-collection',
-			'posts-collection',
-			'card',
-			'google-map',
-			'header',
-			'header-row',
-			'headline',
-			'hero',
-			'logo',
-			'media',
-			'menu-food',
-			'navigation',
-			'opentable',
-			'openhours',
-			'post-comments',
-			'posts-collection',
-			'sharing-overlay',
-			'slideshow',
-			'sidecar',
-			'sidecar-area',
+			'advanced-gallery' => [
+				'name' => 'advanced-gallery',
+				'supports' => [ 'blobs' ],
+			],
+			'announcement-bar' => [
+				'name' => 'announcement-bar',
+			],
+			'cards-collection' => [
+				'name' => 'cards-collection',
+			],
+			'card' => [
+				'name' => 'card',
+			],
+			'google-map' => [
+				'name' => 'google-map',
+			],
+			'header' => [
+				'name' => 'header',
+			],
+			'header-row' => [
+				'name' => 'header-row',
+			],
+			'headline' => [
+				'name' => 'headline',
+			],
+			'hero' => [
+				'name' => 'hero',
+				'supports' => [ 'doppler' ],
+			],
+			'logo' => [
+				'name' => 'logo',
+			],
+			'media' => [
+				'name' => 'media',
+				'supports' => [ 'blobs' ],
+			],
+			'menu-food' => [
+				'name' => 'menu-food',
+			],
+			'navigation' => [
+				'name' => 'navigation',
+			],
+			'opentable' => [
+				'name' => 'opentable',
+			],
+			'openhours' => [
+				'name' => 'openhours',
+			],
+			'post-comments' => [
+				'name' => 'post-comments',
+			],
+			'posts-collection' => [
+				'name' => 'posts-collection',
+			],
+			'sharing-overlay' => [
+				'name' => 'sharing-overlay',
+			],
+			'slideshow' => [
+				'name' => 'slideshow',
+			],
+			'sidecar' => [
+				'name' => 'sidecar',
+			],
+			'sidecar-area' => [
+				'name' => 'sidecar-area',
+			],
 		);
 
 		$anima_novablocks_config = apply_filters( 'anima_novablocks_config', $anima_novablocks_config );
