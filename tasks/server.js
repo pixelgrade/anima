147a455e080fabb57d84f610236ee339d6feaf77
Razvan <razwan.me@gmail.com>
2021-11-22 12:22:14 +0200

    Simplify browserSync config

--- tasks/server.js
+++ tasks/server.js
@@ -1,8 +1,7 @@
-var gulp = require( 'gulp' );
-var fs = require( 'fs' );
-var browserSync = require( 'browser-sync' ).create();
-var gulpconfig;
-var defaults = { open: 'external' };
+const gulp = require( "gulp" );
+const fs = require( 'fs' );
+const browserSync = require( 'browser-sync' ).create();
+let gulpconfig;
 
 if ( fs.existsSync( './tasks/gulpconfig.json' ) ) {
 	gulpconfig = require( './gulpconfig.json' );
@@ -11,39 +10,18 @@ if ( fs.existsSync( './tasks/gulpconfig.json' ) ) {
 	console.warn( "Don't forget to create your own gulpconfig.json from gulpconfig.json.example" );
 }
 
-var config = Object.assign( {}, defaults, gulpconfig.server );
-
-function reload( done ) {
-	browserSync.reload("*.css");
-	done();
-};
-
 gulp.task( 'server', function() {
-	console.log( gulpconfig, config );
-	
-
-	// Provide a callback to capture ALL events to CSS
-	// files - then filter for 'change' and reload all
-	// css files on the page.
-	// https://browsersync.io/docs/api#api-watch
-	browserSync.watch(['*.css', '../../plugins/nova-blocks/,*.css'], function (event, file) {
-	    if (event === 'change') {
-	        browserSync.reload('*.css');
-	    }
-	});
-
-	browserSync.watch('dist/**/*.js').on('change', browserSync.reload);
 
 	// Init the Browsersync server
 	browserSync.init( {
-		watch: true,
+		files: [
+			'../../plugins/nova-blocks/build/**/*',
+			'./dist/**/*',
+			'./style.css',
+		],
+		open: false,
 		proxy: gulpconfig.server.domain,
-		open: 'external',
 		injectChanges: true,
 	} );
-
-	// add browserSync.reload to the tasks array to make
-	// all browsers reload after tasks are complete.
-	// gulp.watch( [ 'dist/**/*.js', '*.css' ], reload );
 
 } );
