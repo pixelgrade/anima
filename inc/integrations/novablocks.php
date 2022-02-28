7bd1e37d5ba3153463705dbd82abcb2f8e5e9873
Razvan <razwan.me@gmail.com>
2022-02-28 17:32:04 +0200

    Add enable => true to all blocks in theme_support config

--- inc/integrations/novablocks.php
+++ inc/integrations/novablocks.php
@@ -21,74 +21,97 @@ if ( ! function_exists( 'anima_novablocks_setup' ) ) {
 			'advanced-gallery' => [
 				'name' => 'advanced-gallery',
 				'supports' => [ 'blobs' ],
+				'enabled' => true,
 			],
 			'announcement-bar' => [
 				'name' => 'announcement-bar',
+				'enabled' => true,
 			],
 			'cards-collection' => [
 				'name' => 'cards-collection',
+				'enabled' => true,
 			],
 			'card' => [
 				'name' => 'card',
+				'enabled' => true,
 			],
 			'google-map' => [
 				'name' => 'google-map',
+				'enabled' => true,
 			],
 			'header' => [
 				'name' => 'header',
+				'enabled' => true,
 			],
 			'header-row' => [
 				'name' => 'header-row',
+				'enabled' => true,
 			],
 			'headline' => [
 				'name' => 'headline',
+				'enabled' => true,
 			],
 			'hero' => [
 				'name' => 'hero',
 				'supports' => [ 'doppler' ],
+				'enabled' => true,
 			],
 			'logo' => [
 				'name' => 'logo',
+				'enabled' => true,
 			],
 			'media' => [
 				'name' => 'media',
 				'supports' => [ 'blobs' ],
+				'enabled' => true,
 			],
 			'menu-food' => [
 				'name' => 'menu-food',
+				'enabled' => true,
 			],
 			'navigation' => [
 				'name' => 'navigation',
+				'enabled' => true,
 			],
 			'opentable' => [
 				'name' => 'opentable',
+				'enabled' => true,
 			],
 			'openhours' => [
 				'name' => 'openhours',
+				'enabled' => true,
 			],
 			'post-comments' => [
 				'name' => 'post-comments',
+				'enabled' => true,
 			],
 			'posts-collection' => [
 				'name' => 'posts-collection',
+				'enabled' => true,
 			],
 			'sharing-overlay' => [
 				'name' => 'sharing-overlay',
+				'enabled' => true,
 			],
 			'slideshow' => [
 				'name' => 'slideshow',
+				'enabled' => true,
 			],
 			'sidecar' => [
 				'name' => 'sidecar',
+				'enabled' => true,
 			],
 			'sidecar-area' => [
 				'name' => 'sidecar-area',
+				'enabled' => true,
 			],
 			'supernova' => [
 				'name' => 'supernova',
+				'enabled' => true,
 			],
 			'supernova-item' => [
 				'name' => 'supernova-item',
+				'enabled' => true,
 			],
 		];
 
