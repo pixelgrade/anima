f439b460d8423d9194477eb7e94f51818345ae3b
Razvan <razwan.me@gmail.com>
2022-03-23 11:37:38 +0200

    Update header block pattern

--- inc/fse/patterns/header-default.php
+++ inc/fse/patterns/header-default.php
@@ -7,12 +7,12 @@ return array(
 	'categories' => array( 'header' ),
 	'blockTypes' => array( 'core/template-part/header' ),
 	'content'    => '<!-- wp:novablocks/header {"layout":"logo-center"} -->
-<!-- wp:novablocks/header-row {"name":"primary","label":"Primary Navigation","isSticky":true,"isPrimary":true,"className":"nb-header-row\u002d\u002dprimary"} -->
-<!-- wp:novablocks/navigation {"slug":"primary","className":"nb-navigation nb-navigation\u002d\u002dprimary"} /-->
+<!-- wp:novablocks/header-row {"slug":"primary","label":"Primary Navigation","isSticky":true,"isPrimary":true} -->
+<!-- wp:novablocks/navigation {"slug":"primary"} /-->
 
 <!-- wp:novablocks/logo /-->
 
-<!-- wp:novablocks/navigation {"slug":"secondary","className":"nb-navigation nb-navigation\u002d\u002dsecondary"} /-->
+<!-- wp:novablocks/navigation {"slug":"secondary"} /-->
 <!-- /wp:novablocks/header-row -->
 <!-- /wp:novablocks/header -->',
 );
