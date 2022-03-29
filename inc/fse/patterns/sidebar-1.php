0d3f7b3075cdd562b7d9828afadfcfcd5622176e
Vlad Olaru <vlad@pixelgrade.com>
2022-03-29 17:33:45 +0300

    Add sidebar parts and logic

---
+++ inc/fse/patterns/sidebar-1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Default sidebar.
+ */
+
+ob_start();
+if ( is_active_sidebar( 'sidebar-1' ) ) {
+	dynamic_sidebar( 'sidebar-1' );
+}
+$sidebar_content = ob_get_clean();
+
+return array(
+	'title'      => __( 'Sidebar Articles (theme)', '__theme_txtd' ),
+	'categories' => array( 'sidebar' ),
+	'blockTypes' => array( 'core/template-part/sidebar' ),
+	'content'    => $sidebar_content,
+);
