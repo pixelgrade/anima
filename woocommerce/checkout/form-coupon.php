a81deed3c76f6851846933d0df740df530c5fa19
George Olaru <george@pixelgrade.com>
2023-02-14 17:09:16 +0200

    Updated templates for WooCommerce 7.1 #338

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -11,30 +11,33 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.4
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
+
 defined( 'ABSPATH' ) || exit;
+
 if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
-	return;
+    return;
 }
+
 ?>
-
 <div class="woocommerce-form-coupon-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', '__theme_txtd' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', '__theme_txtd' ) . '</a>' ), 'notice' ); ?>
+    <?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', '__theme_txtd' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', '__theme_txtd' ) . '</a>' ), 'notice' ); ?>
 </div>
 
-<div class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
 
-	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', '__theme_txtd' ); ?></p>
+    <p><?php esc_html_e( 'If you have a coupon code, please apply it below.', '__theme_txtd' ); ?></p>
 
-	<p class="form-row form-row-first">
-		<input type="text" name="coupon_code" class="js-coupon-value-source input-text" placeholder="<?php esc_attr_e( 'Coupon code', '__theme_txtd' ); ?>" id="coupon_code" value="" form="form-coupon" />
-	</p>
+    <p class="form-row form-row-first">
+        <label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', '__theme_txtd' ); ?></label>
+        <input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', '__theme_txtd' ); ?>" id="coupon_code" value="" />
+    </p>
 
-	<p class="form-row form-row-last">
-		<button type="submit" class="button" form="form-coupon" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', '__theme_txtd' ); ?>"><?php esc_html_e( 'Apply coupon', '__theme_txtd' ); ?></button>
-	</p>
+    <p class="form-row form-row-last">
+        <button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', '__theme_txtd' ); ?>"><?php esc_html_e( 'Apply coupon', '__theme_txtd' ); ?></button>
+    </p>
 
-	<div class="clear"></div>
-</div>
+    <div class="clear"></div>
+</form>
--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -3,8 +3,8 @@
  * Single variation cart button
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -17,16 +17,18 @@ global $product;
 	<?php
 	do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-	woocommerce_quantity_input( [
-		'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-		'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-		'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-	] );
+	woocommerce_quantity_input(
+		array(
+			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+		)
+	);
 
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<button type="submit" class="add_to_cart_button  ajax_add_to_cart"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	<button type="submit" class="add_to_cart_button  ajax_add_to_cart single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
