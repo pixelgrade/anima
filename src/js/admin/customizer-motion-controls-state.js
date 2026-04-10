693dcfe9994c7081e91049337275552d3dad4033
George Olaru <george@pixelgrade.com>
2026-04-10 12:35:34 +0300

    feat: add intro animation motion controls
    Refs #421

--- src/js/admin/customizer-motion-controls-state.js
+++ src/js/admin/customizer-motion-controls-state.js
@@ -1,18 +1,35 @@
-const MOTION_DEPENDENT_CONTROL_IDS = [
-  'sm_page_transition_style',
-  'sm_logo_loading_style',
-  'sm_transition_symbol',
-];
+const MOTION_DEPENDENT_CONTROLS = {
+  sm_page_transitions_enable: [
+    'sm_page_transition_style',
+    'sm_logo_loading_style',
+    'sm_transition_symbol',
+  ],
+  sm_intro_animations_enable: [
+    'sm_intro_animations_style',
+    'sm_intro_animations_speed',
+  ],
+};
 
-function getMotionDependentControlsState(isEnabled) {
-  return MOTION_DEPENDENT_CONTROL_IDS.reduce((state, controlId) => {
-    state[controlId] = !isEnabled;
+const MOTION_DEPENDENT_CONTROL_IDS = Object.values(MOTION_DEPENDENT_CONTROLS).flat();
+
+function normalizeBoolean(value) {
+  return value === true || value === 1 || value === '1' || value === 'true';
+}
+
+function getMotionDependentControlsState(settings = {}) {
+  return Object.entries(MOTION_DEPENDENT_CONTROLS).reduce((state, [settingId, controlIds]) => {
+    const isEnabled = normalizeBoolean(settings[settingId]);
+
+    controlIds.forEach((controlId) => {
+      state[controlId] = !isEnabled;
+    });
 
     return state;
   }, {});
 }
 
 module.exports = {
+  MOTION_DEPENDENT_CONTROLS,
   MOTION_DEPENDENT_CONTROL_IDS,
   getMotionDependentControlsState,
 };
--- test/customizer-motion-controls-state.test.js
+++ test/customizer-motion-controls-state.test.js
@@ -6,17 +6,40 @@ const {
 } = require('../src/js/admin/customizer-motion-controls-state.js');
 
 test('motion-dependent controls are disabled when page transitions are off', () => {
-  const state = getMotionDependentControlsState(false);
+  const state = getMotionDependentControlsState({
+    sm_page_transitions_enable: false,
+    sm_intro_animations_enable: true,
+  });
 
   assert.equal(state.sm_page_transition_style, true);
   assert.equal(state.sm_logo_loading_style, true);
   assert.equal(state.sm_transition_symbol, true);
+  assert.equal(state.sm_intro_animations_style, false);
+  assert.equal(state.sm_intro_animations_speed, false);
 });
 
 test('motion-dependent controls stay enabled when page transitions are on', () => {
-  const state = getMotionDependentControlsState(true);
+  const state = getMotionDependentControlsState({
+    sm_page_transitions_enable: true,
+    sm_intro_animations_enable: true,
+  });
 
   assert.equal(state.sm_page_transition_style, false);
   assert.equal(state.sm_logo_loading_style, false);
   assert.equal(state.sm_transition_symbol, false);
+  assert.equal(state.sm_intro_animations_style, false);
+  assert.equal(state.sm_intro_animations_speed, false);
+});
+
+test('intro-animation controls are disabled when intro animations are off', () => {
+  const state = getMotionDependentControlsState({
+    sm_page_transitions_enable: true,
+    sm_intro_animations_enable: false,
+  });
+
+  assert.equal(state.sm_page_transition_style, false);
+  assert.equal(state.sm_logo_loading_style, false);
+  assert.equal(state.sm_transition_symbol, false);
+  assert.equal(state.sm_intro_animations_style, true);
+  assert.equal(state.sm_intro_animations_speed, true);
 });
