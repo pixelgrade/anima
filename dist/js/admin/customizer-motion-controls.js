7e2ba232565086da9eb198299ff33b8051265c3f
George Olaru <george@pixelgrade.com>
2026-04-10 12:45:33 +0300

    chore: rebuild intro animation assets
    Refs #421

--- dist/js/admin/customizer-motion-controls.js
+++ dist/js/admin/customizer-motion-controls.js
@@ -19,15 +19,19 @@ module.exports = {
 /***/ 290
 (module) {
 
+const MOTION_TOGGLE_SETTING_IDS = ['sm_page_transitions_enable', 'sm_intro_animations_enable'];
 function attachMotionControlsSync(customizeApi, syncDependentControls) {
-  customizeApi('sm_page_transitions_enable', setting => {
-    syncDependentControls(setting.get());
-    setting.bind(value => {
-      syncDependentControls(value);
+  MOTION_TOGGLE_SETTING_IDS.forEach(settingId => {
+    customizeApi(settingId, setting => {
+      syncDependentControls(settingId, setting.get());
+      setting.bind(value => {
+        syncDependentControls(settingId, value);
+      });
     });
   });
 }
 module.exports = {
+  MOTION_TOGGLE_SETTING_IDS,
   attachMotionControlsSync
 };
 
@@ -36,14 +40,25 @@ module.exports = {
 /***/ 143
 (module) {
 
-const MOTION_DEPENDENT_CONTROL_IDS = ['sm_page_transition_style', 'sm_logo_loading_style', 'sm_transition_symbol'];
-function getMotionDependentControlsState(isEnabled) {
-  return MOTION_DEPENDENT_CONTROL_IDS.reduce((state, controlId) => {
-    state[controlId] = !isEnabled;
+const MOTION_DEPENDENT_CONTROLS = {
+  sm_page_transitions_enable: ['sm_page_transition_style', 'sm_logo_loading_style', 'sm_transition_symbol'],
+  sm_intro_animations_enable: ['sm_intro_animations_style', 'sm_intro_animations_speed']
+};
+const MOTION_DEPENDENT_CONTROL_IDS = Object.values(MOTION_DEPENDENT_CONTROLS).flat();
+function normalizeBoolean(value) {
+  return value === true || value === 1 || value === '1' || value === 'true';
+}
+function getMotionDependentControlsState(settings = {}) {
+  return Object.entries(MOTION_DEPENDENT_CONTROLS).reduce((state, [settingId, controlIds]) => {
+    const isEnabled = normalizeBoolean(settings[settingId]);
+    controlIds.forEach(controlId => {
+      state[controlId] = !isEnabled;
+    });
     return state;
   }, {});
 }
 module.exports = {
+  MOTION_DEPENDENT_CONTROLS,
   MOTION_DEPENDENT_CONTROL_IDS,
   getMotionDependentControlsState
 };
@@ -262,6 +277,10 @@ const {
     return;
   }
   const DISABLED_CLASS = 'anima-control-is-disabled';
+  const motionToggleSettings = {
+    sm_page_transitions_enable: false,
+    sm_intro_animations_enable: false
+  };
   const motionPreviewSettings = {
     sm_page_transitions_enable: false,
     sm_page_transition_style: 'border_iris',
@@ -287,8 +306,11 @@ const {
       field.disabled = isDisabled;
     });
   }
-  function syncDependentControls(isEnabled) {
-    const controlState = getMotionDependentControlsState(!!isEnabled);
+  function syncDependentControls(settingId, value) {
+    if (settingId) {
+      motionToggleSettings[settingId] = value;
+    }
+    const controlState = getMotionDependentControlsState(motionToggleSettings);
     MOTION_DEPENDENT_CONTROL_IDS.forEach(controlId => {
       setControlDisabledState(controlId, controlState[controlId]);
     });
