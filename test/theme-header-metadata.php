7cbd7ab0d786b97c63cd683acb0ebc434445dabb
George Olaru <george@pixelgrade.com>
2026-08-13 14:17:48 +0300

    Prepare Anima 2.0.47 release
    Refs #593

--- test/theme-header-metadata.php
+++ test/theme-header-metadata.php
@@ -23,8 +23,8 @@ foreach ( $files as $file ) {
 
 	$contents = file_get_contents( $path );
 
-	if ( ! preg_match( '/^Version:\s*2\.0\.46\s*$/m', $contents ) ) {
-		fwrite( STDERR, sprintf( "%s must declare Version: 2.0.46\n", $file ) );
+	if ( ! preg_match( '/^Version:\s*2\.0\.47\s*$/m', $contents ) ) {
+		fwrite( STDERR, sprintf( "%s must declare Version: 2.0.47\n", $file ) );
 		exit( 1 );
 	}
 
